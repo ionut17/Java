1260671528fde791264084dd44cb79d1b8a908f1
anca9153 <adascalitei.anca@gmail.com>
2016-02-28 12:09:07 +0200

    Lab 2: Problem parameters (with setters/getters) added

--- Laborator 2/src/control/Problem.java
+++ Laborator 2/src/control/Problem.java
@@ -1,9 +1,59 @@
 package control;
 
+import java.util.ArrayList;
+import model.*;
+
 /**
  *
  * @author Anca Adascalitei, Ionut Iacob
  */
 public class Problem {
+    
+    private ArrayList<Student> studentList = new ArrayList<>();
+    private ArrayList<Lecturer> lecturerList = new ArrayList<>();
+    private ArrayList<Project> projectList = new ArrayList<>();
+
+    /**
+     * @return the studentList
+     */
+    public ArrayList<Student> getStudentList() {
+        return studentList;
+    }
+
+    /**
+     * @param studentList the studentList to set
+     */
+    public void setStudentList(ArrayList<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    /**
+     * @return the lecturerList
+     */
+    public ArrayList<Lecturer> getLecturerList() {
+        return lecturerList;
+    }
+
+    /**
+     * @param lecturerList the lecturerList to set
+     */
+    public void setLecturerList(ArrayList<Lecturer> lecturerList) {
+        this.lecturerList = lecturerList;
+    }
+
+    /**
+     * @return the projectList
+     */
+    public ArrayList<Project> getProjectList() {
+        return projectList;
+    }
+
+    /**
+     * @param projectList the projectList to set
+     */
+    public void setProjectList(ArrayList<Project> projectList) {
+        this.projectList = projectList;
+    }
+    
     
 }
