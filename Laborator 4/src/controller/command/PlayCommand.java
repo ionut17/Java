4a796d5341676c929ef00ec31f2f3725b96b9962
ionut17 <ionut.iacob17@gmail.com>
2016-03-15 19:15:29 +0200

    Lab 4: Modified play command, working

--- Laborator 4/src/controller/command/PlayCommand.java
+++ Laborator 4/src/controller/command/PlayCommand.java
@@ -4,22 +4,35 @@ import java.awt.Desktop;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import view.exception.InvalidCommandException;
 
 /**
  *
  * @author Anca Adascalitei, Ionut Iacob
  */
-public class PlayCommand extends AbstractCommand{
+public class PlayCommand extends AbstractCommand {
 
     @Override
     public void execute() throws InvalidCommandException {
         Desktop desktop = Desktop.getDesktop();
         StringBuilder sb = new StringBuilder("\\");
-        for (String s:args){
-            sb.append(s).append(" ");
+        for (int i = 0; i < args.length-1; i++) {
+            sb.append(args[i]).append(" ");
         }
-        Path path = Paths.get(attachedAudioManager.getCurrentDirectory().toString()+sb.toString());
+        sb.append(args[args.length-1]);
+        
+        String str = attachedAudioManager.getCurrentDirectory().toString() + sb.toString();
+        System.out.println(str);
+        
+        String[] list = str.split("\\\\");
+        StringBuilder sb2 = new StringBuilder();
+        for (int i = 0; i < list.length-1; i++) {
+            sb2.append(list[i]).append("\\\\");
+        }
+        sb2.append(list[list.length-1]);
+//        System.out.println(sb2.toString());
+        Path path = Paths.get(sb2.toString());
         System.out.println(path.toString());
         if(path.toFile().exists()) try {
             desktop.open(path.toFile());
@@ -30,5 +43,5 @@ public class PlayCommand extends AbstractCommand{
             throw new InvalidCommandException("File doesn't exist!");
         }
     }
-    
+
 }
