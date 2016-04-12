eeca68c779f6c6499b62b128a0eb517afba475d0
ionut17 <ionut.iacob17@gmail.com>
2016-04-12 17:44:54 +0300

    Lab 7: Added structure snippet

---
+++ Laborator 7/src/lab7/controller/LetterPack.java
@@ -0,0 +1,14 @@
+package lab7.controller;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ *
+ * @author Ionut
+ */
+public class LetterPack{
+    
+    Map<Tile, Integer> pack = new ConcurrentHashMap<>();
+    
+}
---
+++ Laborator 7/src/lab7/controller/Tile.java
@@ -0,0 +1,12 @@
+package lab7.controller;
+
+/**
+ *
+ * @author Ionut
+ */
+class Tile {
+    
+    char letter;
+    int value;
+    
+}
