c5eef14d83a0a754c7a18d601f1c076ccc4ce462
anca9153 <adascalitei.anca@gmail.com>
2016-04-28 11:20:30 +0300

    Merge branch 'master' of https://github.com/ionut17/Java
    Conflicts:
    	Laborator 8/GameServer/src/lab8/server/controller/PlayerThread.java

--- Laborator 9/src/lab9/controller/DatabaseManager.java
+++ Laborator 9/src/lab9/controller/DatabaseManager.java
@@ -4,8 +4,10 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.LayoutManager;
+import java.awt.List;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -23,7 +25,7 @@ import javax.swing.JTextField;
  */
 class DatabaseManager {
 
-    Connection con = null;
+    static Connection con = null;
 
     public DatabaseManager() throws IOException, ClassNotFoundException, SQLException {
         //Making a connection
@@ -32,7 +34,7 @@ class DatabaseManager {
         createGUI();
     }
 
-    private static void createGUI() {
+    private static void createGUI() throws SQLException {
         //Create and set up the window.
         JFrame frame = new JFrame("DatabaseManager");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -43,23 +45,52 @@ class DatabaseManager {
         JPanel left = new JPanel();
         left.setPreferredSize(new Dimension(300, 600));
         left.setLayout(new BorderLayout());
-        
+
         JPanel queries = new JPanel();
-        queries.setLayout(new BoxLayout(queries,BoxLayout.PAGE_AXIS));
+        queries.setLayout(new BoxLayout(queries, BoxLayout.PAGE_AXIS));
+
+        DatabaseMetaData databaseMetaData = con.getMetaData();
+        String catalog = null;
+        String schemaPattern = "STUDENT";
+        String namePattern = null;
+        String[] types = null;
+
+        ArrayList<String> metadata = new ArrayList<>();
+        
+        ResultSet tables = databaseMetaData.getTables(catalog, schemaPattern, namePattern, types);
+        while (tables.next()) {
+           metadata.add(tables.getString("TABLE_SCHEM") + ", " + tables.getString("TABLE_NAME") + ", " + tables.getString("TABLE_TYPE"));
+        }
+
+        ResultSet functions = databaseMetaData.getFunctions(catalog, schemaPattern, namePattern);
+        while (functions.next()) {
+            metadata.add(functions.getString("FUNCTION_SCHEM") + ", " + functions.getString("FUNCTION_NAME") + ", " + functions.getString("FUNCTION_TYPE")+", FUNCTION");
+        }
+
+        ResultSet procedures = databaseMetaData.getProcedures(catalog, schemaPattern, namePattern);
+        while (procedures.next()) {
+            metadata.add(procedures.getString("PROCEDURE_SCHEM") + ", " + procedures.getString("PROCEDURE_NAME") + ", " + procedures.getString("PROCEDURE_TYPE")+", PROCEDURE");
+        }
+
+        String[] array_metadata = new String[metadata.size()];
+        array_metadata=metadata.toArray(array_metadata);  // in array_metadata ai string-uri cu metadatele fiecarui obiect din baza de date :* 
+        for(String s : array_metadata){
+            System.out.println("* "+s);
+        }
         
         queries.add(new JLabel("Table 1"));
         queries.add(new JLabel("Table 2"));
         queries.add(new JLabel("Table 3"));
-                
+
         JPanel inputPanel = new JPanel();
         inputPanel.setLayout(new FlowLayout());
         JTextField input = new JTextField();
-        input.setText();
+        input.setText("jkah");
         inputPanel.add(input);
-        
+
         left.add(queries, BorderLayout.PAGE_START);
         left.add(inputPanel, BorderLayout.CENTER);
-        
+
         frame.add(left, BorderLayout.CENTER);
         frame.add(new JPanel(), BorderLayout.LINE_END);
 
@@ -92,7 +123,6 @@ class DatabaseManager {
         String[] cols = {"nr_matricol", "nume", "prenume"};
 
 //        SimpleAdhocReport arp = new SimpleAdhocReport(cols, values);
-
     }
 
 }
